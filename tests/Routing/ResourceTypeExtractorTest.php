1b590ce83fa7a8da8247f991cb4fac6a9e266d25
Pavel Z <r3volut1oner@gmail.com>
2025-05-06 14:41:28 +0300

    github: improve ci

--- tests/Routing/ResourceTypeExtractorTest.php
+++ tests/Routing/ResourceTypeExtractorTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Routing;
 
 use Illuminate\Routing\Route;
-use PHPUnit\Framework\Attributes\DataProvider;
 use Sowl\JsonApi\Routing\ResourceTypeExtractor;
 use Tests\TestCase;
 
@@ -23,31 +22,31 @@ class ResourceTypeExtractorTest extends TestCase
         $this->assertExtractsResourceType('users/specialCustomEndpoint', 'users');
         $this->assertExtractsResourceType('users', 'users');
         $this->assertExtractsResourceType('users/{id}', 'users');
-        
+
         // Nested and relationship resources
         $this->assertExtractsResourceType('users/{id}/posts', 'users');
         $this->assertExtractsResourceType('users/{id}/relationships/posts', 'users');
         $this->assertExtractsResourceType('users/{userId}/posts/{postId}', 'users');
-        
+
         // Special cases
         $this->assertExtractsResourceType('{resourceType}/{id}', null);
         $this->assertExtractsResourceType('/{dictionaryType}', null);
         $this->assertExtractsResourceType('', null);
-        
+
         // Different naming conventions
         $this->assertExtractsResourceType('blog-posts', 'blog-posts');
         $this->assertExtractsResourceType('user_profiles', 'user_profiles');
-        
+
         // Complex cases
         $this->assertExtractsResourceType(
             'organizations/{orgId}/departments/{deptId}/employees/{empId}/tasks',
             'organizations'
         );
-        
+
         // Routes with parameters
         $this->assertExtractsResourceType('search/{query?}', 'search');
         $this->assertExtractsResourceType('articles/{id:[0-9]+}', 'articles');
-        
+
         // Dot notation
         $this->assertExtractsResourceType('api.v1.users', 'api.v1.users');
     }
